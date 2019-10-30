a40116ba78285d71f19b6ab5a35a6e783878312b
Tom <tsechrist@outlook.com>
2019-10-30 14:10:13 -0500

    Slimming down and removing unnecessary complications

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 
 export const Navigator = () => (
-  <Navbar expand="md">
+  <Navbar expand="md" sticky="top" bg="dark" variant="dark" className="text-light">
       <Navbar.Brand href="/"></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item>
-            <Nav.Link>
-              <Link to="/">About</Link>
-            </Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link>
-              <Link to="/portfolio">Portfolio</Link>
-            </Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link>
-              <Link to="/contact">Contact</Link>
-            </Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
