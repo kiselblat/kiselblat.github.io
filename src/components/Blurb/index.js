89b9d76246744c500602e28b840ba58b85c308e9
Tom <tsechrist@outlook.com>
2019-11-04 10:58:33 -0600

    Adds title prop and prop for justifying pic right

--- src/components/Blurb/index.js
+++ src/components/Blurb/index.js
@@ -5,8 +5,19 @@ import { Layout } from '../';
 export const Blurb = props => {
   return(
     <Layout className={`${props.className} blurb`} style={props.style}>
+      {props.title ?
+        <Row>
+          <Col>
+            <h3>
+              {props.title}
+            </h3>
+          </Col>
+        </Row>
+      :
+        <Row className="d-none" />
+      }
       <Row>
-        {props.src ? 
+        {props.src && !props.right ? 
           <Col md={4}>
               <img src={props.src} alt={props.alt} />
           </Col>
@@ -16,6 +27,13 @@ export const Blurb = props => {
         <Col md>
           <p>{props.children}</p>
         </Col>
+        {props.src && props.right ? 
+          <Col md={4}>
+              <img src={props.src} alt={props.alt} />
+          </Col>
+          :
+          <Col className="d-none" />
+        }
       </Row>
     </Layout>
   )
