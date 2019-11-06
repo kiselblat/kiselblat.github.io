33d29f1048b7b53a48e0411e33c18a5d840f83a9
Tom <tsechrist@outlook.com>
2019-11-06 12:04:41 -0600

    ghpages is just very particular

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -20,13 +20,13 @@ export const Navigator = props => {
       <Navbar.Collapse id="basic-navbar-nav" >
         <Nav className="nav">
           <Nav.Item>
-            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/#">About</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/#/portfolio">Portfolio</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/#/contact">Contact</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
