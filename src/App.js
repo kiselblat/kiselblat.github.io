78814e49a9919fa3c0eaa7d71693ca69f91bb570
Tom <tsechrist@outlook.com>
2019-11-06 11:26:06 -0600

    Trys to get the thing working on ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Header } from './components/Header';
 
@@ -32,7 +32,7 @@ class App extends Component {
   
   render() {
     return(
-      <Router>
+      <Router basename="/">
         <Header
           currentScrollHeight={this.state.currentScrollHeight}
           subtitle={name.title}
