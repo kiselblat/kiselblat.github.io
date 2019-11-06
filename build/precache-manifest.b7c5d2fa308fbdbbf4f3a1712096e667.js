4c06073f77f06284c826fb59b164264c6c97b84f
Tom <tsechrist@outlook.com>
2019-11-06 12:06:09 -0600

    ghpages that's not fair, ghpages just is what it is

---
+++ build/precache-manifest.b7c5d2fa308fbdbbf4f3a1712096e667.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527583c2dac1ca712a6f2868501f2c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "685f95ff02e3b4dfc05d",
+    "url": "/static/css/main.4c798336.chunk.css"
+  },
+  {
+    "revision": "820751f1fb42d6134895",
+    "url": "/static/js/2.b5a33971.chunk.js"
+  },
+  {
+    "revision": "685f95ff02e3b4dfc05d",
+    "url": "/static/js/main.ae472d82.chunk.js"
+  },
+  {
+    "revision": "54a33d24491d7154651c",
+    "url": "/static/js/runtime-main.c210b13c.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  }
+]);
